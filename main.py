2f9da1bc3796325ac453718f778931815d8d8aa0
Frank <frankkovmir@gmail.com>
2022-11-21 22:49:08 +0100

    Sounds ergänzt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Dokumentation: https://docs.python.org/3/library/tk.html
 # weiterhin genutzte Dokumentation: https://www.educba.com/python-tkinter/
+# Pygame Dokumentation für Sounds: https://www.pygame.org/docs/ref/mixer.html
 # Import der benötigten Libraries
 
 import tkinter as tki
@@ -7,6 +8,9 @@ from PIL import ImageTk, Image
 from tkinter import messagebox
 import numpy as np
 import matplotlib
+import pygame
+import time # ggf. notwendig für api unterbrechung bei spam
+
 
 """
 Part 3 - Implementierung der Button - Funktionen
@@ -20,57 +24,72 @@ Part 3 - Implementierung der Button - Funktionen
 def los_button():
     # prüfen, dass radius und kraftstoff gewählt sind
     # prüfen, dass adresse eine PLZ oder Ort + Straße ist
-    #AdressLabel = tki.Label(root, text=adresse.get())  # ruft den gespeicherten Input auf
+    # AdressLabel = tki.Label(root, text=adresse.get())  # ruft den gespeicherten Input auf
     popup()
+
+def ende_button():
+    end_sound = pygame.mixer.Sound(".\sounds\Shutdown.mp3")
+    end_sound.play()
+    response = tki.messagebox.showinfo("Goodbye", "Auf Wiedersehen!")
+    tki.Label(root, text=response)
+    root.quit()
 
 # 3.2 Funktion für die Kontrollfrage und Einstieg in die Output - Funktionen
 
 def popup():
+    popup_sound = pygame.mixer.Sound(".\sounds\Control.mp3")
+    popup_sound.play()
     response = tki.messagebox.askyesno("Bitte bestätigen", "Sind Sie mit der Auswahl einverstanden?")
     tki.Label(root, text=response)
     if response == 1:
         pass
         # if cvs is checked -> weiter in cvs Funktion
-            # cvs_export()
+        # cvs_export()
         # if pdf is checked -> weiter in pdf Funktion
-            # pdf_export()
+        # pdf_export()
         # if streetmap is checked:
-            # map_export()
+        # map_export()
     else:
+        error_sound = pygame.mixer.Sound(".\sounds\Windows.mp3")
+        error_sound.play()
         response = tki.messagebox.showerror("Anfrage gestoppt", "Die Anfrage wurde abgebrochen. Bitte Daten prüfen")
         tki.Label(root, text=response)
+
 
 def ausgabeformat_button():
     pass
 
-# 3.3 Funktion löscht die PLZ - Eingabemaske bei Klick in das Feld Adresse (überschreibt den Default Text)
-# übernommen von https://www.tutorialspoint.com/how-to-clear-text-widget-content-while-clicking-on-the-entry-widget-itself-in-tkinter
+
+# 3.3 Funktion löscht die PLZ - Eingabemaske bei Klick in das Feld Adresse (überschreibt den Default Text) übernommen
+# von https://www.tutorialspoint.com/how-to-clear-text-widget-content-while-clicking-on-the-entry-widget-itself-in
+# -tkinter
 
 def click(event):  # es muss ein parameter in die Funktion übergeben werden
     adresse.configure()
     adresse.delete(0, tki.END)
     adresse.unbind('<Button-1>', clicked)
 
+
 # 3.4 Funktion für die Generierung der Streetmap in einem zweiten Fenster
 
 def map_export():
+    # ggf. sind global variablen notwendig
     newframe = tki.Toplevel()
     newframe.title('Google Streetview')
     root.iconbitmap('./icon/gasstation_4334.ico')
 
-# 3.4 Funktion für die Generierung der Streetmap in einem zweiten Fenster
+
+# 3.5 Funktion für die Generierung des cvs Exports falls gewählt
 
 def cvs_export():
-    newframe = tki.Toplevel()
-    newframe.title('Google Streetview')
-    root.iconbitmap('./icon/gasstation_4334.ico')
+    pass
 
-# 3.4 Funktion für die Generierung der Streetmap in einem zweiten Fenster
+
+# 3.6 Funktion für die Generierung des pdf Exports falls gewählt
 
 def pdf_export():
-    newframe = tki.Toplevel()
-    newframe.title('Google Streetview')
-    root.iconbitmap('./icon/gasstation_4334.ico')
+    pass
+
 
 """
 Part 1 - Erstellen des GUI und der benötigten Buttons / Tabs (Frank Kovmir)
@@ -84,6 +103,11 @@ root = tki.Tk()  # erstellt das Root-Fenster für alle weiteren Widgets (d.h. Bu
 root.title("PKI - Benzinpreisapp")  # bennent das Fenster
 root.geometry("550x600")  # setzt die Maße, Breite x Höhe
 root.iconbitmap('./icon/gasstation_4334.ico')  # Iconanpassung
+pygame.mixer.init() # initialisiert Sounds
+pygame.mixer.music.load(r".\sounds\bg.mp3")
+pygame.mixer.music.set_volume(0.1)
+pygame.mixer.music.play(loops=-1) # unendlicher loop für die Hintergrundmusik
+
 
 # Einfügen eines Hintergrundbildes
 bg = ImageTk.PhotoImage(Image.open("./icon/bg.jpg"))
@@ -107,7 +131,7 @@ adresse.get()  # speichert den Input
 clicked = adresse.bind('<Button-1>', click)
 
 # Ausgabeformat - muss singlechoice - klick ('radiobutton') sein (Wahl: CVS / PDF / Streetmap)
-canvas.create_text(110, 145, text="Ausgabeformat wählen", fill="white", font=('Helvetica 13 bold'))
+canvas.create_text(110, 145, text="Ausgabeformat wählen", fill="white", font='Helvetica 13 bold')
 
 r = tki.IntVar()  # die Variable wird als ein Integer definiert, um später in einer Funktion mit
 # Zahlen arbeiten zu können. Alternativ wäre z.B. auch StrVar() möglich, wenn der value "1" gewählt wird
@@ -120,8 +144,7 @@ output_map = tki.Radiobutton(root, text="Streetmap", variable=r, value=3)
 # Start und Ende - muss normaler Button sein
 los = tki.Button(root, text="Los", padx=60, pady=60,
                  command=los_button)  # Achtung, Funktion fehlt noch
-ende = tki.Button(root, text='Beenden', padx=60, pady=60, command=root.quit)
-
+ende = tki.Button(root, text='Beenden', padx=60, pady=60, command=ende_button)
 
 # 1.3 Plotten der Buttons in das root GUI - Fenster
 
@@ -133,4 +156,6 @@ output_pdf.place(x=20, y=240)
 output_map.place(x=20, y=290)
 los.place(x=380, y=430)
 ende.place(x=20, y=430)
+
+
 root.mainloop()  # führt eine Endlosschleife durch (startet das sichtbare GUI-Fenster)
