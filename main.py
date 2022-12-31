ec63d264552dbf29aef4ceb66a769219ca0f1f9b
Frank <frankkovmir@gmail.com>
2022-12-31 17:02:05 +0100

    Wiederherstellung Main Branch

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ import pgeocode
 import string
 import requests
 import json
-from fpdf import FPDF
 
 #frankchange
 
@@ -239,13 +238,7 @@ def pdf_export(new_list):
         kein Return-Wert
     """
 
-    pdf=FPDF()
-    pdf.add_page()
-    pdf.set_font("Arial",size=12)
-    for i in new_list:
-        pdf.write(5,str(i))
-        pdf.ln() #Einfügen von Zeilenumbrueche
-    pdf.output("./output/ergebnislist.pdf")
+    pass
 
 
 # 3.1.9 Funktion, um nur aktive Tankstellen anzuzeigen. Soll je nach Checkbox Status ein True (checked)
